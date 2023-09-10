a7b27abcfe3434efe1cc9da9e602db014e3c9186
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-10 20:44:50 +0500

    Добавьте файлы проекта.

---
+++ Homework2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Привет создатель,как тебя зовут?");
+string answeruser = Console.ReadLine();
+Console.WriteLine("А как ты назовешь меня?");
+string namebot = Console.ReadLine();
+Console.WriteLine($"Приятно познакомимся {answeruser}.Меня зовут {namebot} - это ты меня назвал");
+Console.ReadKey();
